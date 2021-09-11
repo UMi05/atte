7a43407071cc3dbb88cf71074c9be68e904a6613
KAITO61417 <kaio.o6145@gmail.com>
2021-09-11 21:20:08 +0900

    打刻ボタン処理修正

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -4,90 +4,217 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Models\User;
-use App\Http\Models\Work;
+use App\Models\Work;
+use App\Models\Rest;
 use Carbon\Carbon;
+
 
 class AttendanceController extends Controller
 {
-    public function workIn(Request $request)
+
+
+    public function workIn()
     {
         $user = Auth::user();
+        $workTimestamp = Work::where('user_id', $user->id)->latest()->first();
 
-        // 打刻は１日一回までにしたい
+        //** レコードが空の場合、出勤打刻 */
+        if (!$workTimestamp) {
 
-        $oldTimestamp = Work::where('user_id', $user->id)->latest()->first();
-        if ($oldTimestamp) {
-            $oldTimestampPunchIn = new Carbon($oldTimestamp->start_work);
-            $oldTimestampDay = $oldTimestampPunchIn->startOfDay();
-        } else {
-            $timestamp = Work::create([
+            Work::create([
                 'user_id' => $user->id,
                 'start_work' => Carbon::now(),
+                'attendance_date' => Carbon::today(),
+            ]);
+            return redirect()->back()->with('message', 'おはようございます、本日もよろしくお願いします。');
+
+        }
+
+        /**
+        * 最新のレコードの打刻確認し、打刻されていた場合
+        * 打刻済みの日付取得かつ、その日の日付を取得
+        */
+        if ($workTimestamp) {
+
+            $oldTimestampDay = new Carbon($workTimestamp->attendance_date);
+            $newTimestampDay = Carbon::today();
+
+        }
+
+        //** 同日の出勤打刻の重複を防ぐ */
+        if (($oldTimestampDay == $newTimestampDay) && !$workTimestamp->end_work) {
+
+            return redirect()->back()->with('message', 'すでに出勤打刻がされています。');
+
+        } elseif (($oldTimestampDay == $newTimestampDay) && $workTimestamp->start_work && $workTimestamp->end_work) {
+
+            return redirect()->back()->with('message', '本日は既に業務終了しています。');
+
+        } elseif (($oldTimestampDay !== $newTimestampDay) && !$workTimestamp->end_work) {
+
+            $workTimestamp->update([
+                'end_work' => new Carbon('23:00:00')
             ]);
 
-            return redirect()->back()->with('message', 'おはようございます');
-        }
-
-        $newTimestampDay = Carbon::today();
-
-        // 日付を比較、同日の出勤打刻かつ、直前のTimestampの退勤打刻がされてない場合エラーを吐く
-        if ($oldTimestampDay == $newTimestampDay && (empty($oldTimestamp->end_work))) {
-            return redirect()->back()->with('message', 'すでに出勤打刻がされています');
-        }
-
-        $timestamp = Work::create([
-            'user_id' => $user->id,
-            'start_work' => Carbon::now(),
-        ]);
-
-        return redirect()->back()->with('message', '出勤打刻が完了しました');
-    }
-
-    public function workOut(Request $request)
-    {
-        $user = Auth::user();
-        $timestamp = Work::where('user_id', $user->id)->latest()->first();
-
-        if ( !empty($timestamp->end_work)) {
-            return redirect()->back()->with('message', '既に退勤の打刻がされているか、出勤打刻されていません');
-        }
-        $timestamp->update([
-            'end_work' => Carbon::now()
-        ]);
-
-        return redirect()->back()->with('message', 'お疲れ様でした。');
-    }
-
-    public function restIn(Request $request)
-    {
-        $user = Auth::user();
-        $timestamp = Work::where('user_id', $user->id)->latest()->first();
-
-        // 出勤済み、退勤前、休憩前の場合打刻
-        if ($timestamp->start_work && (empty($timestamp->end_work)) && (empty($timestamp->start_rest))) {
-            $timestamp->update([
-                'start_rest' => Carbon::now()
+            Work::create([
+                'user_id' => $user->id,
+                'start_work' => Carbon::now(),
+                'attendance_date' => Carbon::today(),
             ]);
+            return redirect()->back()->with('message', 'おはようございます、本日もよろしくお願いします。');
+
+        } elseif (($oldTimestampDay !== $newTimestampDay)) {
+
+            Work::create([
+                'user_id' => $user->id,
+                'start_work' => Carbon::now(),
+                'attendance_date' => Carbon::today(),
+            ]);
+            return redirect()->back()->with('message', 'おはようございます、本日もよろしくお願いします。');
+
+        } else {
+
             return redirect()->back();
-        }
 
-        return redirect()->back();
+        }
 
     }
 
-    public function restOut(Request $request)
+
+    public function workOut()
     {
         $user = Auth::user();
-        $timestamp = Work::where('user_id', $user->id)->latest()->first();
+        $workTimestamp = Work::where('user_id', $user->id)->latest()->first();
+        $restTimestamp = Rest::where('work_id', $workTimestamp->id)->latest()->first();
 
-        // 休憩済み、退勤前の場合打刻
-        if($timestamp->start_rest && (empty($timestamp->end_rest))) {
-            $timestamp->update([
+        /**
+         * work テーブルが空（出勤記録がない）または、出勤打刻前
+         * 退勤打刻後、押したときの処理
+         * 休憩終了打刻がされていなかった場合、退勤打刻時間と一緒に打刻
+         * 退勤打刻の処理
+         */
+        if (!$workTimestamp || !$workTimestamp->start_work) {
+
+            return redirect()->back()->with('message', '出勤打刻されていません。');
+
+        } elseif ($workTimestamp->end_work) {
+
+            return redirect()->back()->with('message', '退勤済みです。');
+
+        }elseif ($restTimestamp && !$restTimestamp->end_rest) {
+
+            $restTimestamp->update([
                 'end_rest' => Carbon::now()
             ]);
-            return redirect()->back();
+
+            $workTimestamp->update([
+                'end_work' => Carbon::now()
+            ]);
+            return redirect()->back()->with('message', 'お疲れ様でした。');
+
+        } elseif ($workTimestamp->start_work) {
+
+            $workTimestamp->update([
+                'end_work' => Carbon::now()
+            ]);
+            return redirect()->back()->with('message', 'お疲れ様でした。');
+
         }
-        return redirect()->back();
+
+    }
+
+
+    public function restIn()
+    {
+        $user = Auth::user();
+        $workTimestamp = work::where('user_id', $user->id)->latest()->first();
+
+        //** 出勤前（works table が空の場合のエラー防止） */
+        if (!$workTimestamp) {
+
+            return redirect()->back()->with('message', '出勤打刻されていません。');
+
+        } else {
+            $restTimestamp = Rest::where('work_id', $workTimestamp->id)->latest()->first();
+        }
+
+        /**
+        * 出勤打刻後かつ、work テーブルがからの場合
+        * 休憩終了打刻前の、休憩開始打刻の重複を防ぐ
+        * 既に、休憩済みでも何度も休憩可能にする処理
+        * 退勤打刻後、押した場合の処理
+        */
+
+        if (($workTimestamp->start_work && !$workTimestamp->end_work) && !$restTimestamp) {
+
+            $restTimestamp = Rest::create([
+                'work_id' => $workTimestamp->id,
+                'start_rest' => Carbon::now(),
+            ]);
+            return redirect()->back()->with('message', '休憩開始です、お疲れ様です。');
+
+        } elseif (($workTimestamp->start_work && !$workTimestamp->end_work) && ($restTimestamp->start_rest && !$restTimestamp->end_rest)) {
+
+            return redirect()->back()->with('message', '休憩中です。');
+
+        } elseif(($workTimestamp->start_work && !$workTimestamp->end_work) && $restTimestamp) {
+
+            $restTimestamp = Rest::create([
+                'work_id' => $workTimestamp->id,
+                'start_rest' => Carbon::now(),
+            ]);
+            return redirect()->back()->with('message', '休憩開始です、お疲れ様です。');
+
+        } elseif ($workTimestamp->end_work) {
+
+            return redirect()->back()->with('message', '退勤済みです。');
+
+        }
+
+    }
+
+
+    public function restOut()
+    {
+        $user = Auth::user();
+        $workTimestamp = work::where('user_id', $user->id)->latest()->first();
+
+        //** 出勤前（works table が空の場合のエラー防止） */
+        if (!$workTimestamp) {
+            return redirect()->back()->with('message', '出勤打刻されていません。');
+        } else {
+            $restTimestamp = Rest::where('work_id', $workTimestamp->id)->latest()->first();
+        }
+
+        /**
+        * 休憩のレコードがない場合
+        * 休憩終了の処理
+        * 退勤打刻後、押した場合の処理
+        * 休憩開始前で押したときの処理
+        */
+        if(!$restTimestamp) {
+
+            return redirect()->back()->with('message', '休憩開始されていません。');
+
+        } elseif ($restTimestamp->start_rest && !$restTimestamp->end_rest) {
+
+            $restTimestamp->update([
+                'end_rest' => Carbon::now()
+            ]);
+            return redirect()->back()->with('message', '休憩終了です、引き続きよろしくお願いします。');
+
+        } elseif ($workTimestamp->end_work) {
+
+            return redirect()->back()->with('message', '退勤済みです。');
+
+        } elseif (!$restTimestamp->rest_start) {
+
+            return redirect()->back()->with('message', '休憩開始されていません。');
+
+        } else {
+
+            return redirect()->back();
+
+        }
     }
 }
