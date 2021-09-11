7bd7331b3269ea0b5a35a5ba0cdfd26180451273
KAITO61417 <kaio.o6145@gmail.com>
2021-09-11 21:26:24 +0900

    ボタンのdisable削除

--- resources/views/app/attendance.blade.php
+++ resources/views/app/attendance.blade.php
@@ -2,40 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="greeting py-5">
-            <h1>{{ Auth::user()->name }}さん</h1>
-            @if (session('message'))
-                {{ session('message') }}
-            @endif
-        </div>
+    <div class="name row justify-content-center py-3">
+        <h1>{{ Auth::user()->name }}さん</h1>
+    </div>
+    <div class="msg row justify-content-center py-2">
+        <p>{{ session('message') }}</p>
     </div>
     <div class="atte-btn row justify-content-center">
         <div>
             <form action="{{ route('workin') }}" method="POST">
                 @csrf
-                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                <button type="submit" id="btn1" onclick="workin()" name="workIn" value="workIn">出勤</button>
+                <button type="submit" id="btn1">出勤</button>
             </form>
 
             <form action="{{ route('restin') }}" method="POST">
                 @csrf
-                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                <button type="submit" id="btn3" onclick="restin()" disabled name="restIn" value="restIn">休憩開始</button>
+                <button type="submit" id="btn3">休憩開始</button>
             </form>
         </div>
 
         <div>
             <form action="{{ route('workout') }}" method="POST">
                 @csrf
-                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                <button type="submit" id="btn2" onclick="workout()" disabled name="workOut" value="workOut">退勤</button>
+                <button type="submit" id="btn2">退勤</button>
             </form>
 
             <form action="{{ route('restout') }}" method="POST">
                 @csrf
-                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                <button type="submit" id="btn4" onclick="restout()" disabled name="restOut" value="restOut">休憩終了</button>
+                <button type="submit" id="btn4">休憩終了</button>
             </form>
         </div>
 
