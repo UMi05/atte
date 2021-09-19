a39d2cc68de0ee6a439647ba4a90e4cd345ec00a
KAITO61417 <kaio.o6145@gmail.com>
2021-09-19 19:19:27 +0900

    latest

--- resources/views/check.blade.php
+++ resources/views/check.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="container">
-    {{ $date_users->links() }}
+
     <div class="row justify-content-center">
         <div class="date">
-            @foreach($date_users as $date)
-                <h1>{{ $date->date->date->format('Y-m-d') }}</h1>
+            @foreach($allDate as $date)
+                <h1>{{ $date->date }}</h1>
             @endforeach
         </div>
     </div>
@@ -24,7 +24,7 @@
             <tbody align="center">
                 @foreach ($users as $user)
                         <tr>
-                            <td>{{ $user->user->name }}</td>
+                            <td>{{ $user->name }}</td>
                             <td>{{ $user->start_work }}</td>
                             <td>{{ $user->end_work }}</td>
                         </tr>
