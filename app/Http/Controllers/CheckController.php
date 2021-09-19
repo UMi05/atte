fcea8b9c7487dfbd27f1f9d9af13860f99dcbbda
KAITO61417 <kaio.o6145@gmail.com>
2021-09-19 17:51:33 +0900

    latest

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -4,18 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Date;
+use App\Models\Date_User;
 use App\Models\Work;
 use App\Models\Rest;
 use Carbon\Carbon;
+use DateTime;
+
+use function GuzzleHttp\Promise\all;
 
 class CheckController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        $dates = Work::with('user')->simplePaginate(1);
-        $workTs = Work::with('user')->get();
 
-        return view('/app/check',compact('users', 'dates', 'workTs'));
+        $workTable = Date::get()->first();
+
+        if (!$workTable) {
+            return redirect()->back()->with('message', '勤務履歴がありません。');
+        }
+
+        $allDate = Date_User::join('dates', 'dates.id', 'date_users.date_id')
+        ->select('date')
+        ->orderBy('date', 'asc')
+        ->simplePaginate(1);
+
+        $users = User::join('works', 'works.user_id', 'users.id')
+        ->paginate(5);
+        // dd($users);
+
+        return view('check', compact('allDate', 'users'));
+
     }
+
 }
